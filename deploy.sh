8e43767f7dd3dec45187bcedb77b102fa73543d9
Dowon Cha <dowon1014@gmail.com>
2017-02-13 20:05:13 -0500

    fixed gitconfig

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sudo apt install /
+    vim /
+    zsh /
+    curl /
+
+# Install docker
+    
---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Get current dir
+
+export DOTFILES_DIR EXTRA_DIR
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+EXTRA_DIR="$HOME/.extra"
+
+# update dot files
+
+# symlinks
+
+ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
+ln -sfv "$DOTFILES_DIR/vim/.vimrc" ~
+ln -sfv "$DOTFILES_DIR/zsh/.zshrc" ~
+
